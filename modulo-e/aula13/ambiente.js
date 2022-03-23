3adb659a3f909eefea559930f23e96aac7e11562
GracielleSampaio <bysunshinearts@gmail.com>
2022-03-23 14:53:31 -0300

    aula13

---
+++ modulo-e/aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+
+do{
+  console.log(`Oi ${c}`)
+  c++
+} while( c <= 10)
+
+
+/*
+var c = 1
+
+while (c <= 3){
+  console.log(`Passo ${c}`)
+  c++
+}
+*/
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
